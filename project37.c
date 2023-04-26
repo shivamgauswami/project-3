f9b975fcd19966c957b51a245f5c7dd232cf7220
shivamgauswami <130463011+shivamgauswami@users.noreply.github.com>
2023-04-26 17:52:00 +0530

    Add files via upload

---
+++ project37.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+main()
+{
+	int n,fd=0,sd=1,sum;
+	
+	printf("Enter your number =");
+	scanf("%d",&n);
+	
+	while (n>fd)
+	{
+	
+	printf("%d ",fd);
+	sum=fd+sd;
+	fd=sd;
+	sd=sum;
+	
+    }
+}
